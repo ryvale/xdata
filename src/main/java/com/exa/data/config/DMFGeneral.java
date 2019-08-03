cae879544c5fc78ec63e0a6b901e2d8957e55678
ryvale <valenother@gmail.com>
2019-08-03 13:26:01 +0000

    Comment support - Test OK

--- src/main/java/com/exa/data/config/DMFGeneral.java
+++ src/main/java/com/exa/data/config/DMFGeneral.java
@@ -7,7 +7,7 @@ import com.exa.data.DataReader;
 import com.exa.data.DataWriter;
 import com.exa.data.XADataSource;
 import com.exa.data.config.utils.DMUtils;
-
+import com.exa.data.expression.DCEvaluatorSetup;
 import com.exa.expression.XPOperand;
 
 import com.exa.expression.parsing.Parser.UnknownIdentifierValidation;
@@ -26,7 +26,6 @@ public class DMFGeneral extends DataManFactory {
 		this.defaultType = defaultType;
 		
 		dmuDmf = this;
-		
 	}
 	
 	public DMFGeneral(String defaultType, FilesRepositories filesRepos, Map<String, XADataSource> dataSources, String defaultDataSource, DMUSetup dmuSetup, UnknownIdentifierValidation uiv) {
@@ -75,26 +74,36 @@ public class DMFGeneral extends DataManFactory {
 		dmFactories.put("oracle", dmf);
 	}
 	
+	public DataReader<?> getDataReader(String type, String drName, DCEvaluatorSetup evSetup) throws ManagedException {
+		if(type == null) return getDataReader(drName, evSetup);
+		
+		DataManFactory dmf = dmFactories.get(type);
+		
+		if(dmf == null) throw new ManagedException(String.format("Invalid data reader type '%s' for '%s'", type, drName));
+		
+		return dmf.getDataReader(drName, evSetup);
+	}
+	
 	@Override
 	public DataReader<?> getDataReader(String name, ObjectValue<XPOperand<?>> ovEntity, DMUtils dmu) throws ManagedException {
 		String type = ovEntity.getAttributAsString("type", defaultType);
 		
 		DataManFactory dmf = dmFactories.get(type);
 		
-		if(dmf == null) throw new ManagedException(String.format("Invalid type '%s'", type));
+		if(dmf == null) throw new ManagedException(String.format("Invalid type '%s' for '%s'", type, name));
 		
 		return dmf.getDataReader(name, ovEntity, dmu);
 	}
 
 	@Override
-	public DataWriter<?> getDataWriter(String name, ObjectValue<XPOperand<?>> ovEntity/*, XPEvaluator eval, VariableContext vc*/, DataReader<?> drSource, DMUtils dmu, boolean preventInsertion, boolean preventUpdate) throws ManagedException {
+	public DataWriter<?> getDataWriter(String name, ObjectValue<XPOperand<?>> ovEntity, DataReader<?> drSource, DMUtils dmu, boolean preventInsertion, boolean preventUpdate) throws ManagedException {
 		String type = ovEntity.getAttributAsString("type", defaultType);
 		
 		DataManFactory dmf = dmFactories.get(type);
 		
 		if(dmf == null) throw new ManagedException(String.format("Invalid type '%s'", type));
 		
-		return dmf.getDataWriter(name, ovEntity/*, eval, vc*/, drSource, dmu, preventInsertion, preventUpdate);
+		return dmf.getDataWriter(name, ovEntity, drSource, dmu, preventInsertion, preventUpdate);
 	}
 
 	
